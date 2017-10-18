c7b71e31b27abddc1b709d69b9506ccd0abd4de4
Deterno <32507776+DeternoMC@users.noreply.github.com>
2017-10-18 19:49:14 +1100

    add /survival <player>

--- src/main/java/net/frostedop/frostedopmod/commands/C_survival.java
+++ src/main/java/net/frostedop/frostedopmod/commands/C_survival.java
@@ -14,16 +14,28 @@ import org.bukkit.entity.Player;
 public class C_survival extends FCommand {
 
     public C_survival() {
-        super("survival", "/survival", "", Arrays.asList("gms"));
+        super("survival", "/survival <player>", Arrays.asList("gms"));
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, Player playersender, String label, String[] args) {
 
         if (args.length == 0) {
             sender.sendMessage(ChatColor.YELLOW + "You are now in GMS!");
             ((Player) sender).setGameMode(GameMode.SURVIVAL);
         }
+        return true;
+    }
+        final Player player = getPlayer(args[0]);
+
+        if (player == null) {
+            sender.sendMessage(ChatColor.RED + "Player not found!");
+            return true;
+        }
+            sender.sendMessage(ChatColor.YELLOW + "Setting " + player.getName() + " to game mode survival");
+            player.sendMessage(ChatColor.YELLOW + sender.getName() + " set your game mode to survival");
+            ((Player) player).setGameMode(GameMode.SURVIVAL);
+
         return true;
     }
 }
